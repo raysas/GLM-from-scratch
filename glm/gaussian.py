a65023d8a581352798efaf53bca488e18fd0756e
raysas <rayane.s.adam@gmail.com>
2025-03-21 13:00:32 +0100

    -- qf

--- glm/gaussian.py
+++ glm/gaussian.py
@@ -135,7 +135,7 @@ class LinearModel(GLM):
 def main():
     # --testing on vul data
     df= pd.read_csv('example/vul.csv', index_col=0, sep=' ' )
-    X=df.iloc[:,2]
+    X=df.iloc[:,2:4]
     X=X.to_numpy()
     
     y=df.iloc[:,6]
@@ -144,6 +144,26 @@ def main():
     model=LinearModel(X,y)
     model.fit()
     model.summary()
+
+    # plotting in 3d
+    X=model.X
+    y=model.y
+    y_hat=model.y_hat
+
+    # plot in 3d
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(X[:,1], X[:,2], y, c='r', marker='o', label='True')
+    ax.scatter(X[:,1], X[:,2], y_hat, c='b', marker='x', label='Predicted')
+    ax.set_xlabel('X1')
+    ax.set_ylabel('X2')
+    ax.set_zlabel('Y')
+    plt.legend()
+    plt.show()
 
     # -- successfull
     # --------------------------------------------------
